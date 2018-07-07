551caa7e5050b85ac8b87103b6d958a365e5abe4
Angela Shen <angelashen01@gmail.com>
2018-07-07 15:31:32 -0700

    add searchbar and youtube api search capability

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyAGyv7G4g754tXcVznGOyDadVomsWwKY_w';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'cooking'}, (videos) => {
+            // in ES6, equiv. to this.setState({ videos: videos })
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+
+        );
+    }
 }
 
 export default App;
