902630044d79dc3475461ca24988919b7949280d
Angela Shen <angelashen01@gmail.com>
2018-07-07 18:35:10 -0700

    create video detail component and add video selection capability

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAGyv7G4g754tXcVznGOyDadVomsWwKY_w';
@@ -10,18 +11,26 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null };
 
         YTSearch({key: API_KEY, term: "ocean's 8"}, (videos) => {
             // in ES6, equiv. to this.setState({ videos: videos })
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
 
         );
